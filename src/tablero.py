c1354d1998d708e6bab85688943d872870743b23
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-08-26 18:06:55 -0300

    defino la direccion donde se mueven los colores

--- src/tablero.py
+++ src/tablero.py
@@ -8,15 +8,20 @@ class Tablero:
         self.__barra__ = {ficha1: 0, ficha2: 0}
         self.__fichas_fuera__ = {ficha1: 0, ficha2: 0}
 
-    def tablero_inicial():
-        puntos = [{"color": None, "Cantidad": 0} for i in range(24)]
-        puntos[0]  = {"color": NEGRO,  "cantidad": 2}   
-        puntos[5]  = {"color": BLANCO, "cantidad": 5}   
-        puntos[7]  = {"color": BLANCO, "cantidad": 3}   
-        puntos[11] = {"color": NEGRO,  "cantidad": 5}  
-        puntos[12] = {"color": BLANCO, "cantidad": 5}   
-        puntos[16] = {"color": NEGRO,  "cantidad": 3}  
-        puntos[18] = {"color": NEGRO,  "cantidad": 5}  
-        puntos[23] = {"color": BLANCO, "cantidad": 2} 
+    def tablero_inicial(self):
+        puntos = [{"color": None, "cantidad": 0} for i in range(24)]
+        #negras
+        puntos[0]  = {"color": ficha2,  "cantidad": 2}   
+        puntos[5]  = {"color": ficha1, "cantidad": 5}   
+        puntos[7]  = {"color": ficha1, "cantidad": 3}   
+        puntos[11] = {"color": ficha2,  "cantidad": 5}  
+        #blancas
+        puntos[12] = {"color": ficha1, "cantidad": 5}   
+        puntos[16] = {"color": ficha2,  "cantidad": 3}  
+        puntos[18] = {"color": ficha2,  "cantidad": 5}  
+        puntos[23] = {"color": ficha1, "cantidad": 2} 
         return puntos
     
+    def definir_direccion(self, color: str) -> int:
+        return -1 if color == ficha1 else +1 
+        
