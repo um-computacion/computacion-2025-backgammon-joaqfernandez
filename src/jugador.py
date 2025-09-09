b9612f413b5563d86db22d64321e7c142a0cb90a
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-09-09 19:25:23 -0300

    funcion para ver los movimientos permitidos (sin contar bear-off)

--- src/jugador.py
+++ src/jugador.py
@@ -1,4 +1,4 @@
-from src.tablero import Tablero
+from tablero import Tablero
 
 ficha1 = "BLANCO"
 ficha2 = "NEGRO"
@@ -20,3 +20,12 @@ class Jugador:
     
     def direccion(self, tablero) -> int:
         return tablero.definir_direccion(self.__color__)
+    
+    def movimientos_legales(self, tablero, dados: list[int]) -> list[tuple[int,int,int]]:
+        return [
+            (origen, tablero.lugar_destino(self.__color__, origen, dado), dado)
+            for (origen, color, cantidad) in tablero.iter_puntos()
+            if color == self.__color__ and cantidad > 0
+            for dado in dados
+            if tablero.hay_ficha_o_no(self.__color__, origen, dado)
+    ]
