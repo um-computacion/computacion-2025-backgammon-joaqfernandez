c6b964a2f1b9414c280423d070fe7a66eb53086f
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-10-06 15:01:32 -0300

    idento tablero.puede_reingresar

--- src/tablero.py
+++ src/tablero.py
@@ -87,11 +87,11 @@ class Tablero:
     def puede_reingresar(self, color: str, dado: int) -> bool:
         destino = self.punto_entrada_desde_barra(color, dado)
         punto = self.__puntos__[destino]
-            if punto["cantidad"] == 0:
+        if punto["cantidad"] == 0:
                 return True
-            if punto["color"] == color:
+        if punto["color"] == color:
                 return True
-            return punto["cantidad"] == 1 and punto["color"] != color
+        return punto["cantidad"] == 1 and punto["color"] != color
 
     def aplicar_reingreso(self, color: str, dado: int) -> int:
         if self.__barra__[color] <= 0:
