63eae43c4a188e20fc6474d054b030d097d41442
Joaquin <joal.fernandez@alumno.um.edu.ar>
2025-10-07 21:10:07 -0300

    agrego propertys para juego

--- src/juego.py
+++ src/juego.py
@@ -15,3 +15,28 @@ class BackgammonGame:
         self.__turno_actual__ = None
         self.__ganador__ = None
         self.__dados_disponibles__ = []
+
+    @property
+    def tablero(self) -> Tablero:
+        return self.__tablero__
+    
+    @property
+    def jugador1(self) -> Jugador:
+        return self.__jugador1__
+    
+    @property
+    def jugador2(self) -> Jugador:
+        return self.__jugador2__
+    
+    @property
+    def turno_actual(self) -> Jugador:
+        return self.__turno_actual__
+    
+    @property
+    def ganador(self):
+
+        return self.__ganador__
+    
+    @property
+    def dados_disponibles(self) -> list:
+        return self.__dados_disponibles__
